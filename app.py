898c3b9ea49e30b1685b7410ec84f517bcb60fc1
KingBizzleLord <sarath.sarath76@gmail.com>
2018-04-09 02:53:30 +0530

    add, change and delete functionalities to entries

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 from flask import Flask,redirect,session,url_for,abort
 from flask import Flask, render_template, request
+#from db_setup import init_db
 #from database import db_session
 import os
 from flask_sqlalchemy import SQLAlchemy
 from models import *
+
 
 app = Flask(__name__)
 
@@ -40,7 +42,7 @@ def showSignIn():
     
         if request.form['inputPassword']== user.password :
             session['inputName'] = request.form['inputName']
-            return render_template('table.html')
+            return redirect(url_for('showDepartments'))
         else:
             user = not_found_error
     return render_template('signin.html')
@@ -70,7 +72,7 @@ def showSignIn1():
     
         if request.form['inputPassword']== student.password :
             session['inputID'] = request.form['inputID']
-            return render_template('table1.html')
+            return redirect(url_for('showStudentDetails'))
         else:
             student = not_found_error
     return render_template('signin1.html')
@@ -80,12 +82,177 @@ def showSignIn1():
 def showTables1():
     return render_template('table.html')
 
+@app.route('/showStudents', methods=['GET', 'POST'])
+def showStudents():
+    stud = Student.query.all()
+    return render_template('student.html', stud=stud)
+
+
+
+
+
+
+
+
+#Departments
+
+@app.route('/showDepartments',methods=['POST','GET'])
+def showDepartments():
+    if request.method == 'GET' and session['inputName']:
+        depart = Department.query.all() 
+        return render_template('department.html', depart=depart, sessionid=session['inputName'])
     
+@app.route('/editDepartments',methods=['POST','GET'])
+def editDepartments():
+    
+    if request.method == 'POST':
+        department = Department(request.form['inputDepartmentID'], request.form['inputDepartmentName'])
+        db.session.add(department)
+        db.session.commit() 
+        return redirect(url_for('showDepartments'))
 
 
-@app.teardown_appcontext
-def shutdown_session(exception=None):
-    db_session.remove()
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('department1.html')
+
+@app.route('/changeDepartments',methods=['POST','GET'])
+def changeDepartments():
+    if request.method == 'POST':
+        changedepart = Department.query.filter_by(departmentID=request.form['inputDepartmentID']).first()
+        changedepart.departmentName = request.form['inputDepartmentName']
+        db.session.commit() 
+        return redirect(url_for('showDepartments'))
+
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('department2.html')
+
+@app.route('/deleteDepartments',methods=['POST','GET'])
+def deleteDepartments():
+    if request.method == 'POST':
+        deletedepart = Department.query.filter_by(departmentID=request.form['inputDepartmentID']).delete()
+        db.session.commit()
+        return redirect(url_for('showDepartments'))
+
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('department3.html')
+
+
+
+
+
+
+
+#Courses
+
+@app.route('/showCourses',methods=['POST','GET'])
+def showCourses():
+    if request.method == 'GET' and session['inputName']:
+        cours = Course.query.all()  
+        return render_template('course.html', cours=cours)
+
+@app.route('/editCourses',methods=['POST','GET'])
+def editCourses():
+    if request.method == 'POST':
+        course = Course(request.form['inputCourseID'], request.form['inputCourseName'], request.form['inputCredits'], request.form['inputDepartment'])
+        db.session.add(course)
+        db.session.commit() 
+        return redirect(url_for('showCourses'))
+
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('course1.html')
+
+
+@app.route('/changeCourses',methods=['POST','GET'])
+def changeCourses():
+    if request.method == 'POST':
+        changecours = Course.query.filter_by(courseID=request.form['inputCourseID']).first()
+        changecours.courseName = request.form['inputCourseName']
+        changecours.credits = request.form['inputCredits']
+        changecours.department = request.form['inputDepartment']
+        db.session.commit() 
+        return redirect(url_for('showCourses'))
+
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('course2.html')
+
+@app.route('/deleteCourses',methods=['POST','GET'])
+def deleteCourses():
+    if request.method == 'POST':
+        deletecours = Course.query.filter_by(courseID=request.form['inputCourseID']).delete()
+        db.session.commit()
+        return redirect(url_for('showCourses'))
+
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('course3.html')
+#obj = user.query.filter_by(courseID=courseID).first()
+#obj.courseName =  request.form['inputCourseName']
+
+
+#StudentCourse
+
+@app.route('/showStudentCourses',methods=['POST','GET'])
+def showStudentCourses():
+    if request.method == 'GET' and session['inputName']:
+        studcours = StudentCourse.query.all() 
+        return render_template('studentcourse.html', studcours=studcours)
+
+@app.route('/editStudentCourses',methods=['POST','GET'])
+def editStudentCourses():
+    if request.method == 'POST':
+        studentcourse = StudentCourse(request.form['inputCourseName'], request.form['inputStudentName'], request.form['inputMarks'], request.form['inputAttendance'])
+        db.session.add(studentcourse)
+        db.session.commit() 
+        return redirect(url_for('showStudentCourses'))
+
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('studentcourse1.html')
+
+@app.route('/changeStudentCourses',methods=['POST','GET'])
+def changeStudentCourses():
+    if request.method == 'POST':
+        changestudcours = StudentCourse.query.filter_by(courseName=request.form['inputCourseName']).first()
+        changestudcours.studentName = request.form['inputStudentName']
+        changestudcours.marks = request.form['inputMarks']
+        changestudcours.attendance = request.form['inputAttendance']
+        db.session.commit() 
+        return redirect(url_for('showStudentCourses'))
+
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('studentcourse2.html')
+
+@app.route('/deleteStudentCourses',methods=['POST','GET'])
+def deleteStudentCourses():
+    if request.method == 'POST':
+        deletestudcours = StudentCourse.query.filter_by(courseName=request.form['inputCourseName']).delete()
+        db.session.commit()
+        return redirect(url_for('showStudentCourses'))
+
+    if request.method == 'GET' and session['inputName']:    
+        return render_template('studentcourse3.html')
+
+
+@app.route('/showStudentDetails',methods=['POST','GET'])
+def showStudentDetails():
+    if request.method == 'GET' and session['inputID']:
+        studdet = StudentCourse.query.all() 
+        return render_template('studentdetails.html', studdet=studdet,sessionid=session['inputID'])
+
+
+
+@app.route('/logout',methods=['POST','GET'])
+def logout():
+    session.pop('inputName')
+    return redirect(url_for('main'))
+
+@app.route('/studentLogout',methods=['POST','GET'])
+def studentLogout():
+    session.pop('inputID')
+    return redirect(url_for('main'))
+
+    
+#@app.teardown_appcontext
+#def shutdown_session(exception=None):
+ #   db_session.remove()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -42,6 +42,46 @@ class Student(Base):
         self.address = address
         self.phone = phone
 
+class Course(Base):
+    __tablename__ = 'Courses'
+    
+    courseID = db.Column(db.String(50), primary_key=True)
+    courseName = db.Column(db.String(50))
+    credits = db.Column(db.Integer())
+    department = db.Column(db.String(50))
+
+    def __init__(self, courseID, courseName, credits,department):
+        self.courseID = courseID
+        self.courseName = courseName
+        self.credits = credits
+        self.department = department
+
+class StudentCourse(Base):
+    __tablename__='studentCourses'
+    courseName = db.Column(db.String(50), primary_key=True)
+    studentName =  db.Column(db.String(50))
+    marks = db.Column(db.Integer())
+    attendance = db.Column(db.Integer())
+
+    def __init__(self,courseName,studentName, marks,attendance):
+        self.courseName = courseName
+        self.studentName = studentName
+        self.marks = marks
+        self.attendance = attendance
+
+
+class Department(Base):
+    __tablename__ = 'Departments'
+    
+    departmentID = db.Column(db.String(50), primary_key=True)
+    departmentName = db.Column(db.String(50))
+
+    def __init__(self, departmentID, departmentName):
+        self.departmentID = departmentID
+        self.departmentName = departmentName
+
+
+
    # def is_authenticated(self):
     #    return True
 
